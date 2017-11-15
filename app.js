4840c1b0a3e21a2b660cccddb473f6ffcd42006e
Felix <Felix.Huang@student.csulb.edu>
2017-11-14 16:45:13 -0800

    massive changes to the UI. It looks a little bit more like the actual events page.

--- app.js
+++ app.js
@@ -1,19 +1,48 @@
 $(document).ready(function() {
     
       $('#calendar').fullCalendar({
-          weekends: true,
+
+        eventRender: function(events,element) {
+            if(events.type == 'normal') {
+                element.css('background-color','red');
+            }
+        },
+
+  
+        handleWindowResize: true,  
+        weekends: true,
           defaultView: 'month',
+
+          
   
           header: {
               left: 'prev,next',
                   center: 'title',
-                  right : 'basicDay,basicWeek,month'
+                  right : 'basicDay,basicWeek,month,listMonth'
               
                   
-          }
+          },
   
+ // https://stackoverflow.com/questions/36841659/filtering-fullcalendar-events-with-checkboxes-client-side-with-javascript 
   
-  
+ events: [{
+  start: '2017-11-05',
+  title: 'Example 1',
+  type: 'normal'
+}, 
+
+{
+  start: '2017-11-14',
+  title: 'Example 2',
+  type: 'normal'
+}, 
+
+{
+  start:'2017-11-28',
+  title: 'Example 3',
+  type: 'not-normal'
+}]
+
   
   
       });
